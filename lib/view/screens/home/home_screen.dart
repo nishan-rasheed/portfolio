e71e9a16b0ab453d95e24504ba56002f22420c23
nishan-rasheed <nishanrasheed786@gmail.com>
2024-02-07 08:47:46 +0530

    homescreen

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ,
+      appBar:AppBar() ,
       body:SingleChildScrollView(
         child: Column(
           children: [
